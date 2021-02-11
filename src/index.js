2a47b7a615d81d3536f3db316043937235c9fd94
AliaxeiSehiyenia <alexeisergienia@gmail.com>
2021-02-12 00:02:15 +0300

    feat: change function toReadable 0 - 99 and 120 - 999

--- src/index.js
+++ src/index.js
@@ -31,11 +31,11 @@ module.exports = function toReadable (number) {
             if (str_unit == '') {
                 return string = str_unit_hundred + ' ' + 'hundred' + ' ' + str_ten;
             }
-            else if (str_ten == '') {
+            /* else if (str_ten == '') {
                 return string = str_unit_hundred + ' ' + 'hundred' + ' ' + str_unit;
             }
             else if (str_ten == '' && str_unit == '') {
                 return string = str_unit_hundred + ' ' + 'hundred';
-            }
+            } */
         }
 }
