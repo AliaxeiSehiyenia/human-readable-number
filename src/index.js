e45f93379ee9abdb8edfc8f149e68276fae53321
AliaxeiSehiyenia <alexeisergienia@gmail.com>
2021-02-11 22:26:54 +0300

    feat: change function toReadable 0 - 19

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 module.exports = function toReadable (number) {
-        arr_numbers[1] = new Array('', 'one', 'two', 'three', 'four', 'five', 'six', 
+    var arr_numbers = ['', 'one', 'two', 'three', 'four', 'five', 'six', 
         'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 
-        'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen');
+        'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+
         /*_arr_numbers[2] = new Array('', '', 'twenty', 'thirty', 'forty', 'fifty', 
         'sixty', 'seventy', 'eighty', 'ninety'); */
         /*  _arr_numbers[3] = new Array('', 'сто', 'двести', 'триста', 'четыреста', 'пятьсот', 'шестьсот', 'семьсот', 'восемьсот', 'девятьсот'); */
-       
         if (number == 0) {
             return string = "zero";
         }
         else if (number >= 1 && number <= 19) {
-            return string = arr_numbers[1][number];
+            return string = arr_numbers[number];
         }      
 }
