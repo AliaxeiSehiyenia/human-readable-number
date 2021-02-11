4dbe15b616a01d29172dda3405d63c90ca01203b
AliaxeiSehiyenia <alexeisergienia@gmail.com>
2021-02-12 00:16:19 +0300

    feat: change function toReadable 0 - 99 and 120 - 999

--- src/index.js
+++ src/index.js
@@ -5,14 +5,14 @@ module.exports = function toReadable (number) {
     var arr_tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 
         'sixty', 'seventy', 'eighty', 'ninety'];
 
-        /*  _arr_numbers[3] = new Array('', 'сто', 'двести', 'триста', 'четыреста', 'пятьсот', 'шестьсот', 'семьсот', 'восемьсот', 'девятьсот'); */
-        
         if (number == 0) {
             return string = "zero";
         }
+
         else if (number >= 1 && number <= 19) {
             return string = arr_units[number];
         }
+
         else if (number >= 20 && number <= 99) {
             var array_number = number.toString();
             var str_ten = arr_tens[array_number.charAt(0)];
@@ -22,6 +22,11 @@ module.exports = function toReadable (number) {
             }
             else return string = str_ten + ' ' + str_unit;
         }
+
+        else if (number >= 100 && number <= 119) {
+            
+        }
+
         else if (number >= 120 && number <= 999) {
             var array_number = number.toString();
             var str_unit_hundred = arr_units[array_number.charAt(0)];
